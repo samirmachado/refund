3bc0e77b4211adc19847f6fa6492bf4cee6b0a10
Samir Machado de Oliveira <samircomputacao@gmail.com>
2017-10-22 06:42:18 -0200

    cors

--- src/main/java/br/com/zup/refund/CorsConfiguration.java
+++ src/main/java/br/com/zup/refund/CorsConfiguration.java
@@ -1,11 +1,14 @@
 package br.com.zup.refund;
 
-import com.google.common.collect.ImmutableList;
+import java.util.Arrays;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import com.google.common.collect.ImmutableList;
 
 @Configuration
 public class CorsConfiguration {
@@ -17,11 +20,8 @@ public class CorsConfiguration {
         config.setAllowCredentials(true);
         ImmutableList.of("*").forEach(origin -> config.addAllowedOrigin(origin));
         ImmutableList.of("POST", "GET", "PUT", "OPTIONS", "DELETE").forEach(method -> config.addAllowedMethod(method));
-        ImmutableList.of(
-                "token", "content-type"
-        ).forEach(
-                header -> config.addAllowedHeader(header)
-        );
+        ImmutableList.of("token", "content-type").forEach(header -> config.addAllowedHeader(header));
+        config.setExposedHeaders(Arrays.asList("token","Content-Type", "Accept", "X-Requested-With"));
         source.registerCorsConfiguration("/**", config);
         FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
         bean.setOrder(0);
